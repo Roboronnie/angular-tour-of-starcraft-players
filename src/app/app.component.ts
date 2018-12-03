29728be284f79f0de83e995aa57a8e6357efcf20
Mai Yer Lee <mylee@datasci.com>
2018-12-03 15:10:30 -0600

    replace default angular generated UI to display title with some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-starcraft-players';
+  title = 'Tour Starcraft Players';
 }
